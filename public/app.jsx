7e1f94ce47ab1c07119fcb86c58ffdd9bb7fa8b4
augustoerico <augusto.ericosilva@gmail.com>
2017-02-14 09:41:44 -0200

    Finish lesson 12

--- public/app.jsx
+++ public/app.jsx
@@ -1,15 +1,25 @@
 var Greeter = React.createClass({
+  getDefaultProps: function() {
+    return {
+      name: 'React',
+      message: 'This is the default message'
+    }
+  },
   render: function() {
+    var name = this.props.name;
+    var message = this.props.message;
     return (
       <div>
-        <h1>Hello, React from component!</h1>
-        <p>This is from the component</p>
+        <h1>Hello, {name} from component!</h1>
+        <p>{message}</p>
       </div>
     );
   }
 });
 
+var firstName = 'Erico';
+var message = 'Hey, this is from a component!'
 ReactDOM.render(
-  <Greeter/>,
+  <Greeter name={firstName} message={message}/>,
   document.getElementById('app')
 );
